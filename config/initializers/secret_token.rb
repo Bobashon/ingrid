35ac95783bf9996abf04e0119346d70474d2a529
Ingrid Pinedo Pantoja <pinedopingrid@hotmail.com>
2013-05-14 20:37:43 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ingrid::Application.config.secret_token = '906d8630d9d4234cfc08bb6403c597f84c88284b5993094b5d868eff516fe1b2c60599be643ad6fcd6fd62071f902473cf5ac96faf33123fee95351eaffdc4e4'
